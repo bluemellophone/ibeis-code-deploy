aa2e62de25e9d42d73d9c34c6d107d5e0f369468
Jason Parham <bluemellophone@gmail.com>
2016-02-24 19:05:04 -0500

    added sudos and directory deletes

--- scripts/ibeis-ia-cloudformation.sh
+++ scripts/ibeis-ia-cloudformation.sh
@@ -4,8 +4,8 @@ export WAITHANDLE=$1
 # Helper function.
 function error_exit
 {
-  cfn-signal -e 1 -r $1 $WAITHANDLE
-  exit 1
+    sudo cfn-signal -e 1 -r "$1" $WAITHANDLE
+    exit 1
 }
 # Check parameters
 if [ $# -ne 3 ]; then
@@ -30,19 +30,25 @@ sudo apt-get -y install python-setuptools || error_exit 'Failed apt-get install 
 # yes | sudo aptdcon --install python-setuptools || error_exit 'Failed aptdcon install python-setuptools.'
 sudo pip install -U awscli || error_exit 'Failed pip install awscli.'
 # Install the AWS CloudFormation Agent.
-mkdir aws-cfn-bootstrap-latest
-curl https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz | tar xz -C aws-cfn-bootstrap-latest --strip-components 1 || error_exit 'Failed to download AWS CloudFormation Agent.'
-easy_install aws-cfn-bootstrap-latest || error_exit 'Failed to install AWS CloudFormation Agent.'
+if [ -d aws-cfn-bootstrap-latest ]; then
+    sudo rm -rf aws-cfn-bootstrap-latest
+fi
+sudo mkdir aws-cfn-bootstrap-latest
+sudo curl https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz | sudo tar xz -C aws-cfn-bootstrap-latest --strip-components 1 || error_exit 'Failed to download AWS CloudFormation Agent.'
+sudo easy_install aws-cfn-bootstrap-latest || error_exit 'Failed to install AWS CloudFormation Agent.'
 # Install the AWS CodeDeploy Agent.
-mkdir aws-cd-install-latest
+if [ -d aws-cd-install-latest ]; then
+    sudo rm -rf aws-cd-install-latest
+fi
+sudo mkdir aws-cd-install-latest
 cd aws-cd-install-latest
-aws s3 cp --region $REGION s3://aws-codedeploy-$REGION/latest/install . || error_exit 'Failed to download AWS CodeDeploy Agent.'
+sudo aws s3 cp --region $REGION s3://aws-codedeploy-$REGION/latest/install . || error_exit 'Failed to download AWS CodeDeploy Agent.'
 sudo chmod +x ./install
 sudo ./install auto || error_exit 'Failed to install AWS CodeDeploy Agent.'
 # Start the stack initialization
-cfn-init -s $STACKID -r LinuxEC2Instance --region $REGION || error_exit 'Failed to run cfn-init.'
+sudo cfn-init -s $STACKID -r LinuxEC2Instance --region $REGION || error_exit 'Failed to run cfn-init.'
 # All is well, so signal success to the stack.
-cfn-signal -e 0 -r 'AWS CloudFormation and CodeDeploy Agents setup complete.' $WAITHANDLE
+sudo cfn-signal -e 0 -r 'AWS CloudFormation and CodeDeploy Agents setup complete.' $WAITHANDLE
 cd ..
 # sudo rm -rf init.config init.log
 # sudo rm -rf aws-cd-install-latest
